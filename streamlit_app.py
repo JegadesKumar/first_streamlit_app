d3e36a123fa73a73719ce775372453b7085155ab
Jegades K Achappan <118147135+JegadesKumar@users.noreply.github.com>
2022-11-13 22:22:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text(:egg:'Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 🥗 🐔 🥑🍞 Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌 Build Your Own Fruit Smoothie 🥝🍇')
